fc1091c3bae3714be2695b2b8c5baf3ce2890db2
Udomomo <batzuma@yahoo.co.jp>
2018-05-20 22:13:04 +0900

    amountForメソッドをRentalに移した。それに伴い冗長な変数thisAmountをなくした。

--- Customer.java
+++ Customer.java
@@ -33,10 +33,7 @@ public class Customer {
         
         while (rentals.hasMoreElements()) {
             
-            double thisAmount = 0;
             Rental each = rentals.nextElement();
-            
-            thisAmount = amountFor(each);
             
             //レンタルポイントを加算
             frequentRenterPoints ++;
@@ -47,8 +44,8 @@ public class Customer {
             }
             
             //この貸出に対する数値の表示
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
             
         }
         
@@ -59,33 +56,4 @@ public class Customer {
         
     }
     
-    private double amountFor(Rental aRental) {
-        
-        double result = 0;
-        
-        switch (aRental.getMovie().getPriceCode()) {
-            
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-                
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        
-        }
-        
-        return result;
-        
-    }
 }
